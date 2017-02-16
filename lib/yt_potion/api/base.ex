455ae75c44e902fdfaaeb19f5a0b41663d27def4
Bruce <brucepark0@gmail.com>
2017-02-15 17:43:15 -0800

    Create a config.ex to restructure files
    - Add config.ex
    - Rename YtPotion to YtPotion.Base to separate out HTTP functionality
      from config

---
+++ lib/yt_potion/api/base.ex
@@ -0,0 +1,12 @@
+defmodule YtPotion.Base do
+  use HTTPoison.Base
+
+  def base_url(path) do
+    "https://www.googleapis.com/youtube/v3/#{path}"
+  end
+
+  @spec get_request(String.t, map) :: map #HTTPoison.Response{}
+  def get_request(url_part, query_opts) do
+    HTTPoison.get(base_url(url_part), [], params: Map.merge(query_opts, %{key: YtPotion.config(:yt_api_key)}))
+  end
+end
